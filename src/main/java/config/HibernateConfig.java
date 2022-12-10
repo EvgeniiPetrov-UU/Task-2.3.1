7851b47ee004e77e9f7d678d515db42709514f78
EvgeniiPetrov-UU <YOKoSo.anon@yandex.ru>
2022-12-10 18:57:06 +0800

    Configured DB connection

--- src/main/java/config/HibernateConfig.java
+++ src/main/java/config/HibernateConfig.java
@@ -1,4 +1,60 @@
 package config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+@PropertySource("classpath:db.properties")
+@EnableTransactionManagement
+@ComponentScan("java")
 public class HibernateConfig {
+
+    @Autowired
+    private Environment env;
+
+    @Bean
+    public DataSource getDataSource() {
+        DriverManagerDataSource driverMDS = new DriverManagerDataSource();
+        driverMDS.setDriverClassName(env.getProperty("db.driver"));
+        driverMDS.setUrl(env.getProperty("db.url"));
+        driverMDS.setUsername(env.getProperty("db.username"));
+        driverMDS.setPassword(env.getProperty("db.password"));
+        return driverMDS;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean getEntityManagerFactoryBean() {
+        LocalContainerEntityManagerFactoryBean entityManager =
+                new LocalContainerEntityManagerFactoryBean();
+        entityManager.setDataSource(getDataSource());
+        entityManager.setPackagesToScan("java");
+        entityManager.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+
+        Properties prop = new Properties();
+        prop.put("hibernate.show.sql", env.getProperty("hibernate.show.sql"));
+        prop.put("hibernate.hbm2ddl.auto", env.getProperty("hibernate.hbm2ddl.auto"));
+
+        entityManager.setJpaProperties(prop);
+        return entityManager;
+    }
+
+    @Bean
+    public PlatformTransactionManager getTransactionManager() {
+        JpaTransactionManager jpaTransactionManager = new JpaTransactionManager();
+        jpaTransactionManager.setEntityManagerFactory(getEntityManagerFactoryBean().getObject());
+        return jpaTransactionManager;
+    }
 }
